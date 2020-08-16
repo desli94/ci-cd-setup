939f9e84034bf1532bf45a58aa8576cc0539d45d
Desli Mane <desli.developer@gmail.com>
2020-08-16 23:49:19 +0200

    feat(main page): show numbers 0 to 9

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.get("/", (req, res) => {
     <h2>Welcome!</h2>
     <h4>This is the main page</h4>
     <p>a b c d e f g h i j k l m n o p q r s t u v x y z</p>
+    <p>0 1 2 3 4 5 6 7 8 9</p>
   `)
 })
 
