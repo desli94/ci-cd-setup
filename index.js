0e510afbc814282468606d14b32efcb8a8048b16
Desli Mane <desli.developer@gmail.com>
2020-08-16 21:04:47 +0200

    change the environment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { app } = require("./app")
 
-app.listen(8000, () => {
-  console.log("Server running on port 8000...")
+app.listen(3000, () => {
+  console.log("Server running on port 3000...")
 })
