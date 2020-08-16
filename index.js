e2ed841ddfaefc4f3c510233900672b31139ea5b
Desli Mane <desli.developer@gmail.com>
2020-08-16 17:53:26 +0200

    listen on port 80

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { app } = require("./app")
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000...")
+app.listen(80, () => {
+  console.log("Server running on port 80...")
 })
