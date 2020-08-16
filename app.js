b38cf61a4cb40a9ee87c7e12576ccd7b8ce8d2d8
Desli Mane <desli.developer@gmail.com>
2020-08-16 21:59:16 +0200

    alphabet

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ app.use(express.json())
 
 
 app.get("/", (req, res) => {
-  res.send("It's working")
+  res.send(`
+    <h2>Welcome!</h2>
+    <h4>This is the main page</h4>
+    <p>a b c d e f g h i j k l m n o p q r s t u v x y z</p>
+  `)
 })
 
 app.get("/something", (req, res) => {
